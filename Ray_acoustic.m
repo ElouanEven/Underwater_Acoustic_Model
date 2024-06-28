535b44c1a3e5770c14ec332d40f7c3d48f1a707a
ElouanEven <163019334+ElouanEven@users.noreply.github.com>
2024-06-28 11:31:04 +0300

    Update Ray_acoustic.m
    - fix the length vector in ray trace
    - reducing the plotting time

--- Ray_acoustic.m
+++ Ray_acoustic.m
@@ -4,30 +4,28 @@ clear all; close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 tic
 
-Z_src = -50;    % Source depth
-REC_r = 3500;   % Receiver distance
-REC_z = -55;    % Receiver depth
+Z_src = -35;    % Source depth
+REC_r = 4000;   % Receiver distance
+REC_z = -40;    % Receiver depth
 width = 10;     % Receiver width
 f     = 1000;   % Frequency
-theta_aperture = 80;  % Aperture angle (in degree)
-num_rays = 200; % Number of rays to plot
+theta_aperture = 90;  % Aperture angle (in degree)
+num_rays = 1000; % Number of rays to plot
 
 surface_z = 0;  % Surface location
 H = -200;       % Depth
-W = 4000;       % Maximum range
+W = 4500;       % Maximum range
 
 rho_air   = 1.225; % air density
 rho_water = 1028;  % water density
 rho_sand  = 1850;  % sand density
 
 % Updated sound speed profile
-depths   = [   0,  -10,  -20,  -35,  -65,  -80, -100, -200];
-c_values = [1500, 1480, 1470, 1450, 1440, 1435, 1420, 1450];
-c = @(z) interp1(depths, c_values, z, 'linear'); % Interpolated sound speed function
+depths   = [   0,  -10,  -20,  -35,  -65,  -80, -140, -200];
+c_values = [1500, 1480, 1470, 1450, 1455, 1465, 1480, 1500]; %[1500, 1480, 1470, 1450, 1440, 1435, 1420, 1450];
+c = @(z) interp1(depths, c_values, z, 'spline'); % Interpolated sound speed function
 dt = 0.001;         % Time step
-%absorption = 0.1;   % Absorption (dB per meter)
-absorption = (3.3e-3 + (0.11*f^2)/(1+f^2) + (44*f^2)/(4100+f^2) +3e-4*f^2)*1e-3; 
-
+absorption = (3.3e-3 + (0.11*f^2)/(1+f^2) + (44*f^2)/(4100+f^2) +3e-4*f^2)*1e-3; % Absorption (dB per meter)
 
 % Initialize intensity range for color mapping
 log_intensity_min = inf;
@@ -89,22 +87,33 @@ plot([0, W], [surface_z, surface_z], 'k', 'LineWidth', 2);  % Surface
 plot([0, W], [H, H], 'k', 'LineWidth', 2);  % Bottom
 plot([REC_r; REC_r], [REC_z; REC_z+width], 'r', 'LineWidth', 4);  % Receiver
 % Color mapping based on log intensity
-colormap('jet');
-JET = jet(256);
+colormap('gray');
+JET = gray(256);
+j=0;
+% % for i = 1:num_rays
+% %     r = rays_r{i};
+% %     z = rays_z{i};
+% %     log_intensity = rays_log_intensity{i};
+% %     j=j+1;
+% %     %color_idx = 256 - floor((delay_i(j) - min(delay_i)) / (max(delay_i) - min(delay_i)) * 255);
+% %     %color = JET(color_idx, :);  % Get color from colormap based on log intensity
+% %     line(r, z);%, 'Color', color);  % Apply color to line
+% % end
+
 for i = i_ray
     r = rays_r{i};
     z = rays_z{i};
     log_intensity = rays_log_intensity{i};
-    color_idx = floor((log_intensity - log_intensity_min) / (log_intensity_max - log_intensity_min) * 255) + 1;
+    j=j+1;
+    color_idx = 256 - floor((delay_i(j) - min(delay_i)) / (max(delay_i) - min(delay_i)) * 255);
     color = JET(color_idx, :);  % Get color from colormap based on log intensity
-    for j = 1:length(r)-1
-        line([r(j), r(j+1)], [z(j), z(j+1)], 'Color', color(j,:));  % Apply color to line
-    end
+    line(r, z, 'Color', color);  % Apply color to line
 end
 
 
+
 % Add color bar for intensity
-caxis([log_intensity_min, log_intensity_max]);
+%caxis([min(delay_i), max(delay_i)]);
 colorbar;
 ylabel(colorbar, 'Log Intensity');
 
@@ -117,7 +126,6 @@ hold on;
 for i = 1:length(delay_t)
     plot(delay_t(i), delay_i(i), 'o', 'Color', JET(Delay_color(i), :), 'MarkerSize', 8, 'LineWidth', 2);
 end
-caxis([min(nbr_reflexion(i_ray)), max(nbr_reflexion(i_ray))]);
 hold off;
 
 
@@ -129,51 +137,59 @@ fprintf('----- Receiver : %.2f s -----\n', toc);
 %% %%%%%%% Functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [r, z, log_intensity, nbr_reflexion] = ray_trace(theta, Z_src, H, c, W, absorption, dt)
-    
+    MAX_STEP = floor(W/(c(Z_src)*dt) * 1.5); % Estimation of the max number of increments with theta_aperture < 90
+    r = zeros(1,MAX_STEP);
+    z = zeros(1,MAX_STEP);
+    intensity = zeros(1,MAX_STEP);
+
     % Initialize the ray path and intensity
-    r = [    0,         c(Z_src)*cosd(theta)*dt];
-    z = [Z_src, Z_src + c(Z_src)*sind(theta)*dt];
-    intensity = [1, 10^(-absorption * c(z(end))*dt / 10)];
+    r(1:2) = [    0,         c(Z_src)*cosd(theta)*dt];
+    z(1:2) = [Z_src, Z_src + c(Z_src)*sind(theta)*dt];
+    intensity(1:2) = [1, 10^(-absorption * c(z(end))*dt / 10)];
     count_ref = 0;
-    
-    while r(end) < W  % While the ray is in the rectangle
+    i = 2;
+    while (r(end) < W && i < MAX_STEP)  % While the ray is in the rectangle
         
         % Actual conditions
-        last_c    = c(z(end-1)); % Sound speed at last depth
-        current_c = c(z(end  )); % Sound speed at current depth
+        last_c    = c(z(i-1)); % Sound speed at last depth
+        current_c = c(z(i  )); % Sound speed at current depth
 
         if abs((current_c/last_c)*cosd(theta)) < 1 % total transmission 
             theta = sign(theta)*acosd((current_c/last_c)*cosd(theta));
         else % total reflexion
             theta = -sign(theta)*acosd((last_c/current_c)*cosd(theta));
         end
-
+        
         % Calculate increments in r and z
         dr = current_c * cosd(theta) * dt;
         dz = current_c * sind(theta) * dt;
         
         % Check for reflection
-        if z(end) + dz > 0          % on surface
-            r = [r, r(end) - z(end)/tand(theta),   r(end) + dr ];
-            z = [z,                            0, -(z(end) + dz)];
+        if z(i) + dz > 0          % on surface
+            r(i+1:i+2) = [r(i) - z(i)/tand(theta), r(i) + dr ];
+            z(i+1:i+2) = [0, -(z(i) + dz)];
             theta=-theta;
-            intensity_ref = intensity(end) * 10^(-absorption * c(z(end))*dt / 10);
-            intensity = [intensity, intensity_ref, intensity_ref];
+            intensity_ref = intensity(i) * 10^(-absorption * c(z(i))*dt / 10);
+            intensity(i+1:i+2) = [intensity_ref, intensity_ref];
             count_ref = count_ref + 1;
+            i=i+2;
 
-        elseif z(end) + dz < H      % on bottom
-            r = [r, r(end) + (H-z(end))/tand(theta),       r(end) + dr ];
-            z = [z,                                H, -(z(end)+dz) + 2*H];
+        elseif z(i) + dz < H      % on bottom
+            r(i+1:i+2) = [r(i) + (H-z(i))/tand(theta), r(i) + dr ];
+            z(i+1:i+2) = [H, -(z(i)+dz) + 2*H];
             theta=-theta;
-            intensity_ref = intensity(end) * 10^(-absorption * c(z(end))*dt / 10);
-            intensity = [intensity, intensity_ref, intensity_ref];
+            intensity_ref = intensity(i) * 10^(-absorption * c(z(i))*dt / 10);
+            intensity(i+1:i+2) = [intensity_ref, intensity_ref];
             count_ref = count_ref + 1;
+            i=i+2;
 
         else    % Nominal case
-            r = [r, r(end) + dr];
-            z = [z, z(end) + dz];
-            intensity = [intensity, intensity(end) * 10^(-absorption * c(z(end))*dt / 10)];
+            r(i+1) = r(i) + dr;
+            z(i+1) = z(i) + dz;
+            intensity(i+1) = intensity(i) * 10^(-absorption * c(z(i))*dt / 10);
+            i=i+1;
         end
+
     end
     log_intensity = log10(intensity);
     nbr_reflexion = count_ref;
@@ -199,3 +215,4 @@ function [i_ray, delay_t, delay_i] = Cross_receiver(rays_r, rays_z, rays_log_int
         end
     end
 end
+
